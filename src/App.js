29af4cf0a5314ce76ab19d95787eae6ba35722cb
Caleb Schwarze <ces55739@bethel.edu>
2020-10-23 15:03:17 -0500

    Added in the basic calculator component with react

--- src/App.js
+++ src/App.js
@@ -2,25 +2,107 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CalculatorButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.props.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.handleClick.bind(this, this.props.value)}>{this.props.display}</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+class Calculator extends React.Component {
+  render() {
+    const test = "test";
+    return (
+      <div>
+          <table id="visual-calculator">
+              <tbody>
+                  <tr>
+                      <td><CalculatorButton
+                          value="1"
+                          display="1" />
+                      </td>
+                      <td><CalculatorButton
+                          value="2"
+                          display="2" />
+                      </td>
+                      <td><CalculatorButton
+                          value="3"
+                          display="3" />
+                      </td>
+                      <td><CalculatorButton
+                          value=" + "
+                          display="+" />
+                      </td>
+                  </tr>
+                  <tr>
+                      <td><CalculatorButton
+                          value="4"
+                          display="4" />
+                      </td>
+                      <td><CalculatorButton
+                          value="5"
+                          display="5" />
+                      </td>
+                      <td><CalculatorButton
+                          value="6"
+                          display="6" />
+                      </td>
+                      <td><CalculatorButton
+                          value=" - "
+                          display="-" />
+                      </td>
+                  </tr>
+                  <tr>
+                      <td><CalculatorButton
+                          value="7"
+                          display="7" />
+                      </td>
+                      <td><CalculatorButton
+                          value="8"
+                          display="8" />
+                      </td>
+                      <td><CalculatorButton
+                          value="9"
+                          display="9" />
+                      </td>
+                      <td><CalculatorButton
+                          value=" * "
+                          display="x" />
+                      </td>
+                  </tr>
+                  <tr>
+                      <td><CalculatorButton
+                          value="0"
+                          display="0" />
+                      </td>
+                      <td><CalculatorButton
+                          value=" / "
+                          display="÷" />
+                      </td>
+                      <td><CalculatorButton
+                          value=" = "
+                          display="=" />
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+    )
+  }
+}
+
+export default Calculator;
